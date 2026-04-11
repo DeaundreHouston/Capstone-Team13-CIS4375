46f5958fa8033867af58c8f217a05ccd02e0a410
warrenpg <143150322+warrenpg@users.noreply.github.com>
2026-04-10 21:41:01 -0500

    cognito login functionality

---
+++ frontend/auth-config.js
@@ -0,0 +1,9 @@
+
+window.AUTH_CONFIG = {
+  cognitoDomain: "https://us-east-1l91cmq2hb.auth.us-east-1.amazoncognito.com",
+  clientId: "64an5cpadtqhe7kgf0ivgmc787",
+  redirectUri: "http://localhost:5500/frontend/staff-orders.html",
+  logoutUri: "http://localhost:5500/frontend/staff_login.html",
+  ownerGroup: "cheatDay_owner",
+  employeeGroup: "cheatDay_employees"
+};
