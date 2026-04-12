cbbd724dfc8bfd561ce3e309f2a3dc8111ef74ea
warrenpg <143150322+warrenpg@users.noreply.github.com>
2026-04-12 18:29:20 -0500

    push it

--- frontend/auth-config.js
+++ frontend/auth-config.js
@@ -1,8 +1,16 @@
 window.AUTH_CONFIG = {
+
   cognitoDomain: "https://us-east-1l91cmq2hb.auth.us-east-1.amazoncognito.com",
+
   clientId: "64an5cpadtqhe7kgf0ivgmc787",
-  redirectUri: "http://localhost:5500/frontend/staff-orders.html",
-  logoutUri: "http://localhost:5500/frontend/staff_login.html",
+
+  redirectUri: "https://main.d3izqoj4p7aa12.amplifyapp.com/staff-orders.html",
+
+  logoutUri: "https://main.d3izqoj4p7aa12.amplifyapp.com/staff_login.html",
+
   ownerGroup: "cheatDay_owner",
+
   employeeGroup: "cheatDay_employees"
+
 };
+ 
